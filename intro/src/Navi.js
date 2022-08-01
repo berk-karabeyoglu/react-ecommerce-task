761920600226f0203a23c1093e20607bbd5346c1
berk-karabeyoglu <berkkarabeyoglu@gmail.com>
2022-08-01 09:16:18 +0300

    first commit

---
+++ intro/src/Navi.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import CartSummary from "./CartSummary";
+
+export default class Navi extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">React Practice</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink>
+                  <Link to="form1">Nav Link 1</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="form2">Nav Link 2</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Nav Link</NavLink>
+              </NavItem>
+              <CartSummary
+                removeFromCart={this.props.removeFromCart}
+                cart={this.props.cart}
+              />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
